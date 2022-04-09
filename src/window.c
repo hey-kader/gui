b3e971c8cd425d88bb178fb3921883339e4487c7
kader <kaderarnold@icloud.com>
2022-04-09 12:28:13 -0400

    added button

--- src/window.c
+++ src/window.c
@@ -1,13 +1,28 @@
 #include <gtk/gtk.h>
 
-static void activate (GtkApplication* app, gpointer user_data) {
+void clicked (void);
+
+void activate (GtkApplication* app, gpointer user_data) {
 
   GtkWidget *window;
+  GtkWidget *button;
+
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "kader");
-  gtk_window_set_default_size (GTK_WINDOW (window), 512, 400);
-  gtk_widget_show (window);
+  gtk_window_set_default_size (GTK_WINDOW (window), 512, 600);
+
+
+	button = gtk_button_new_with_label (" ");
+	g_signal_connect (button, "clicked", G_CALLBACK (clicked), NULL);
+	gtk_window_set_child(GTK_WINDOW (window), button);
+
+	gtk_window_present(GTK_WINDOW (window));
+
 }
+void clicked () {
+	printf("no touch!\n");
+}
+
 
 int main (int argc, char *argv[]) {
 
